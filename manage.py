a244dfa962ee1fbc74ceeadf16b56d8ca5b6fab7
suke <suke971219@gmail.com>
2024-07-16 11:12:11 +0800

    /

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import better_exceptions
+better_exceptions.hook()
 
 def main():
     """Run administrative tasks."""
